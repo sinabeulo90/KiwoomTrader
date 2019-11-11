265de957e735c27c7e9572db8614fb15384002f4
SeongMok Byeon <smbyun0214@gmail.com>
2019-11-11 20:38:53 +0900

    add opt10004 - opt10008

--- trader.py
+++ trader.py
@@ -9,6 +9,12 @@ class KWTrader(KWCore):
         self.tr_list['opt10001'] = Opt10001(self)
         self.tr_list['opt10002'] = Opt10002(self)
         self.tr_list['opt10003'] = Opt10003(self)
+        self.tr_list['opt10004'] = Opt10004(self)
+        self.tr_list['opt10005'] = Opt10005(self)
+        self.tr_list['opt10006'] = Opt10006(self)
+        self.tr_list['opt10007'] = Opt10007(self)
+        self.tr_list['opt10008'] = Opt10008(self)
+
 
     def connection(self):
         self.comm_connect()
@@ -27,3 +33,28 @@ class KWTrader(KWCore):
     #  [ opt10003 : 체결정보요청 ]
     def opt10003(self, code, prev_next, screen_no):
         return self.tr_list['opt10003'].tr_opt(code, prev_next, screen_no)
+
+
+    # [ opt10004 : 주식호가요청 ]
+    def opt10004(self, code, prev_next, screen_no):
+        return self.tr_list['opt10004'].tr_opt(code, prev_next, screen_no)
+
+
+    # [ opt10005 : 주식일주월시분요청 ]
+    def opt10005(self, code, prev_next, screen_no):
+        return self.tr_list['opt10005'].tr_opt(code, prev_next, screen_no)
+
+
+    # [ OPT10006 : 주식시분요청 ]
+    def opt10006(self, code, prev_next, screen_no):
+        return self.tr_list['opt10006'].tr_opt(code, prev_next, screen_no)
+
+
+    # [ opt10007 : 시세표성정보요청 ]
+    def opt10007(self, code, prev_next, screen_no):
+        return self.tr_list['opt10007'].tr_opt(code, prev_next, screen_no)
+
+
+    # [ opt10008 : 시세표성정보요청 ]
+    def opt10008(self, code, prev_next, screen_no):
+        return self.tr_list['opt10008'].tr_opt(code, prev_next, screen_no)
