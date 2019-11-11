d54b5089c9a365a881ec7bb91a38fa16259ab8b4
SeongMok Byeon <smbyun0214@gmail.com>
2019-11-11 20:38:06 +0900

    add OPT10006 : 주식시분요청

---
+++ tr_option/opt10006.py
@@ -0,0 +1,41 @@
+from tr_option.base import KWTR
+
+# [ OPT10006 : 주식시분요청 ]
+class Opt10006(KWTR):
+
+    def __init__(self, core):
+        super().__init__(core)
+
+        self.rq_name = self.tr_code = "opt10006"
+        self.record_name = '주식시분'
+        self.header = [
+            '날짜', '시가', '고가', '저가', '종가', '대비', '등락률', '거래량', '거래대금', '체결강도',
+        ]
+
+
+    def tr_opt(self, code, prev_next, screen_no):
+
+        self.core.set_input_value("종목코드", code)
+        self.core.comm_rq_data(self.rq_name, self.tr_code, prev_next, screen_no)
+
+        return self.core.receive_tr_data_handler
+
+
+    def tr_opt_data(self, tr_code, rq_name, index):
+        ret = {
+            'header' : self.header,
+            'rows' : [
+                [ self.core.get_comm_data(tr_code, rq_name, index, column) for column in self.header ]
+            ]
+        }
+
+        return ret
+
+
+    def tr_opt_data_ex(self, tr_code, rq_name):
+        ret = {
+            'header' : self.header,
+            'rows' : self.core.get_comm_data_ex(tr_code, rq_name)
+        }
+
+        return ret
